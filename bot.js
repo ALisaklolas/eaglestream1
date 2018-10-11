9c7cc0b93bcd4161f2d7f4e2a6539c0946149ae4
ALisaklolas <35742714+ALisaklolas@users.noreply.github.com>
2018-10-11 23:08:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag} !`);
+ 
+});
+ 
+
 var PrEfix = "-";
 client.on('message', message => {
   if (!message.content.startsWith(PrEfix)) return;
@@ -17,7 +23,7 @@ client.user.setActivity(argresult, {type:'LISTENING'});
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 } else 
 if (message.content.startsWith(PrEfix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/amira");
+  client.user.setGame(argresult, "https://www.twitch.tv/baktash");
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 }
 
